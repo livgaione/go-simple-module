3ea718296fee8a74539c4b112ea3765731500ea7
lgaione <livia.gaione@mercadolivre.com>
2025-05-26 18:30:25 -0300

    feat: weather exercise

--- weather/main.go
+++ weather/main.go
@@ -5,16 +5,15 @@ import "fmt"
 func main() {
 
 	var (
-		
-		temperature int 
-		humidity 	int
-		pressure 	int
-	) 
+		temperature int
+		humidity    int
+		pressure    int
+	)
 
 	temperature = 25
 	humidity = 70
 	pressure = 1013
 
 	fmt.Println("The current weather is", temperature, "degrees Celsius,", humidity, "percent humidity, and", pressure, "hPa pressure.")
-	
+
 }
